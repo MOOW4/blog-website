94a9460692f69bfb62c0a1219d4e7bd5e025c56c
MOOW <christianfeliks1@gmail.com>
2023-09-18 16:38:15 +0200

    add the begining of the implementation of mongoose in get and post routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -20,13 +21,26 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
+
+const postSchema = new mongoose.Schema({
+  title: {
+		type: String,
+		required: true,
+	},
+  body: {
+		type: String,
+		required: true,
+	}
+});
+const Post = mongoose.model("Post", postSchema);
 
 
 app.get("/", (req,res) => {
   //! Won't work  res.sendFile(__dirname + "/views/home.ejs"); 
-  res.render("home", {contentHome : homeStartingContent, posts: posts});
-
-  
+  Post.find({}).then((foundPosts) => {
+    res.render("home", {contentHome : homeStartingContent, posts: foundPosts});
+  });
 });
 
 app.get("/about", (req,res) => {
@@ -45,20 +59,22 @@ app.get("/compose", (req,res) => {
 });
 
 app.post("/compose", (req,res) => {
-/*   console.log(req.body.titleSubmit);
-  console.log(req.body.postSubmit); */
-  const post = {
-    title: req.body.titleSubmit,
-    body: req.body.postSubmit 
-  };
-  posts.push(post);
-  //console.log(posts[0].title);
+
+  const postTitle = req.body.titleSubmit;
+  const postBody = req.body.postSubmit;
+
+  const newPost = new Post ({
+    title: postTitle,
+    body: postBody
+  });
+
+  newPost.save();
   res.redirect("/");
 });
 
 app.get("/posts/:postName", (req,res) => {
-  //console.log(req.params.postName);
   let postName = req.params.postName;
+  //TODO: FIX THIS
   posts.forEach((post) => {
     if(_.lowerCase(post.title) == _.lowerCase(postName)){
       console.log("Match found !");
